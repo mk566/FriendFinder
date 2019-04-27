85701d3412c753159238430c2e07a71e0f6a15a8
mk566 <mk566@nau.edu>
2019-04-27 12:20:44 -0700

    push

---
+++ app/data/friends.js
@@ -0,0 +1,118 @@
+var friends = [
+    {
+      name: "Lisa",
+      photo: "https://www.pexels.com/photo/woman-wearing-pink-collared-half-sleeved-top-1036623/",
+      scores: [
+        "5",
+        "1",
+        "2",
+        "2",
+        "1",
+        "5",
+        "5",
+        "1",
+        "1",
+        "1"
+      ]
+    },
+    {
+      name: "Anne",
+      photo: "https://www.pexels.com/photo/women-s-white-and-black-button-up-collared-shirt-774909/",
+      scores: [
+        "5",
+        "1",
+        "5",
+        "2",
+        "2",
+        "2",
+        "3",
+        "2",
+        "2",
+        "2"
+      ]
+    },
+    {
+      name: "Hailey",
+      photo: "https://www.pexels.com/photo/closeup-photo-of-woman-with-brown-coat-and-gray-top-733872/",
+      scores: [
+        "3",
+        "5",
+        "3",
+        "2",
+        "4",
+        "4",
+        "3",
+        "4",
+        "1",
+        "3"
+      ]
+    },
+    {
+      name: "Jenny",
+      photo: "https://www.pexels.com/photo/woman-wearing-blue-shawl-lapel-suit-jacket-1036622/",
+      scores: [
+        "5",
+        "1",
+        "1",
+        "1",
+        "4",
+        "3",
+        "3",
+        "1",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Roni",
+      photo: "https://www.pexels.com/photo/woman-in-black-scoop-neck-shirt-smiling-38554/",
+      scores: [
+        "5",
+        "5",
+        "1",
+        "2",
+        "1",
+        "2",
+        "3",
+        "4",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Amanda",
+      photo: "https://www.pexels.com/photo/woman-in-brown-crop-top-standing-between-concrete-walls-1138409/",
+      scores: [
+        "1",
+        "1",
+        "2",
+        "4",
+        "3",
+        "4",
+        "2",
+        "4",
+        "1",
+        "2"
+      ]
+    },
+    {
+      name: "Tracey",
+      photo: "https://www.pexels.com/photo/woman-wearing-black-eyeglasses-1239291/",
+      scores: [
+        "1",
+        "1",
+        "2",
+        "2",
+        "1",
+        "1",
+        "2",
+        "1",
+        "1",
+        "1"
+      ]
+    }
+    
+  ];
+  
+  // export friends object
+  module.exports = friends;
